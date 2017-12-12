ca7b54f8979bd9a193e2826cd8a2aaa06b5ffcbc
Samuel Sanft <sdsanft@gmail.com>
2017-12-12 08:09:57 -0700

    Java update

---
+++ Java/Sam1/src/Head.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+
+/**
+ * @author ss016231
+ *
+ */
+public class Head {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello, World!");
+	}
+
+}
