a99b52d1c946416d5019132dc8e31f468a36541e
Samuel Sanft <sdsanft@gmail.com>
2017-12-14 08:46:35 -0700

    new project: game store

---
+++ Java/StoreProjectCP2/src/head.java
@@ -0,0 +1,10 @@
+
+public class head {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		StoreObject[] Inventory = {new StoreObject (), new StoreObject (), new StoreObject ()};
+		System.out.println("New store Opening!");
+	}
+
+}
