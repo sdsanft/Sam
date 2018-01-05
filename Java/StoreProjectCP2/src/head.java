154aceb3480c6c5b756ff5bfae239319271cd80a
Samuel Sanft <sdsanft@gmail.com>
2018-01-05 07:43:55 -0700

    1/5/2017

--- Java/StoreProjectCP2/src/head.java
+++ Java/StoreProjectCP2/src/head.java
@@ -1,10 +1,18 @@
-
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
 public class head {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		StoreObject[] Inventory = {new StoreObject (), new StoreObject (), new StoreObject ()};
+	public static void main(String[] args) throws FileNotFoundException {
+		StoreObject[] Inventory = {new StoreObject ("Game 1", 5.0, 10.0), new StoreObject ("Game 2", 5.0, 10.0), new StoreObject ("Game 3", 5.0, 10.0)};
 		System.out.println("New store Opening!");
+		
+		File f = new File("H:/My Stuff/Java/StoreProjectCP2/src/testing.txt");
+		Scanner s = new Scanner(f);
+		for (int i = 0; i < 4; i++)
+		{
+			System.out.println(s.nextLine());
+		}
+		s.close();
 	}
-
 }
